270bf6287559ec4ec15b74a3504b535a5f22bf3f
Jai Lovehart <jailovehart@gmail.com>
2022-07-06 00:03:25 -0400

    im gonna start crying

--- src/commonTest/kotlin/com/mewsic/common/lang/Tests.kt
+++ src/commonTest/kotlin/com/mewsic/common/lang/Tests.kt
@@ -5,26 +5,7 @@ import kotlin.test.assertTrue
 
 class Tests {
     @Test
-    fun LogWorks() {
-        try {
-            Log.d("Test")
-            assertTrue(true)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            assertTrue(false, "Log.d() failed")
-        }
+    fun test() {
+        assertTrue(true)
     }
-    @Test
-    fun TimePasses() {
-        val runningTime by time.since(System.currentTimeMillis())
-        var somethingToDo = 0
-        for (i in 0..1000000) {
-            somethingToDo += i
-        }
-        assertTrue(
-            runningTime.inWholeMilliseconds > 0
-        )
-
-    }
-
 }
